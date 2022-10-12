f565c60e00f53f35b1749e8f509d68a620c4473b
alessiomodonesi <alessio.modonesi@iisviolamarchesini.edu.it>
2022-10-12 11:42:45 +0200

    es 7 e 8 da finire

--- 5F/EsLab/es7/es7.c
+++ 5F/EsLab/es7/es7.c
@@ -8,11 +8,38 @@
 
 #define SIZE 100
 
-void Function()
+void PrintChar(char *string)
 {
+    int num = 0, s = 0;
+
+    printf("\nLa tua stringa contiene:");
+    for (int i = 0; i < strlen(string); i++)
+    {
+        char c = toupper(string[i]);
+
+        if (i == 0)
+        {
+            num++;
+            printf("\nchar '%c', %d ripetizione/i", c, num);
+        }
+
+        if (string[i] == string[++s] && i > 0)
+        {
+            num++;
+            printf("\nchar '%c', %d ripetizione/i", c, num);
+        }
+        else if (i > 0)
+            printf("\nchar '%c', %d ripetizione/i", c, num);
+    }
 }
 
-int main(int argc, char *argv)
+int main()
 {
-    return 0;
+    char string[SIZE];
+
+    printf("\nInserisci la tua stringa: ");
+    scanf("%s", string);
+
+    PrintChar(string);
+    printf("\n\n");
 }
--- 5F/EsLab/es8/es8.c
+++ 5F/EsLab/es8/es8.c
@@ -8,11 +8,67 @@
 
 #define SIZE 100
 
-void Function()
+void Sort(char *string)
 {
+    int j = 0, k = 0;
+    char str[SIZE];
+    char num[SIZE];
+    char sc[SIZE];
+
+    for (int i = 0; i < strlen(string); i++)
+    {
+        if (isalpha(string[i]) == 1) // lettera maiuscola
+        {
+            for (int j = i + 1; j < strlen(string); j++)
+            {
+                if (string[i] > string[j] && isalpha(string[j]) != 0)
+                {
+                    int c = string[i];
+                    string[i] = string[j];
+                    string[j] = c;
+                    str[i] = c;
+                }
+            }
+        }
+        else if (isalpha(string[i]) == 2) // lettera minuscola
+        {
+            for (int j = i + 1; j < strlen(string); j++)
+            {
+                if (string[i] > string[j] && isalpha(string[j]) == 0)
+                {
+                    int c = string[i];
+                    string[i] = string[j];
+                    string[j] = c;
+                    str[i] = c;
+                }
+            }
+        }
+        else if (isdigit(string[i]) == 1) // numero
+        {
+            num[j] = string[i];
+            j++;
+        }
+        else if (isalpha(string[i]) == 0) // carattere speciale
+        {
+            sc[k] = string[i];
+            k++;
+        }
+    }
+
+    if (j > 0)
+        string = strcat(str, num);
+    if (k > 0)
+        string = strcat(str, sc);
 }
 
-int main(int argc, char *argv)
+int main()
 {
-    return 0;
+    char string[SIZE];
+
+    printf("\nInserisci la tua string: ");
+    scanf("%s", string);
+
+    Sort(string);
+    printf("\nSorted string: %s", string);
+    printf("\n\n");
 }
