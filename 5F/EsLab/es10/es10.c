c22e3491dc015e5ab8153a8b7c70a84122fdbc9f
alessiomodonesi <alessio.modonesi@iisviolamarchesini.edu.it>
2022-10-16 16:34:43 +0200

    eliminati commenti

--- 5F/EsLab/es10/es10.c
+++ 5F/EsLab/es10/es10.c
@@ -1,9 +1,3 @@
-/*FILA B:
-1) Le frequenze dei numeri inseriti e/o generati;
-2) Elimini i numeri che si ripetono più di una volta all’interno del vettore, poi aggiustando la dimensione del vettore affinché risulti pieno;
-3) Ordini, partendo dal vettore iniziale, i numeri dispari in ordine crescente e i pari in ordine
-decrescente.*/
-
 // Alessio Modonesi 5F
 #include <stdio.h>
 #include <stdlib.h>
