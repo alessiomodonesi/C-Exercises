71e16f629def79a9481647fa15b8cf91383e13ba
alessiomodonesi <alessio.modonesi@iisviolamarchesini.edu.it>
2022-10-22 10:03:43 +0200

    .

--- 5F/Socket/client.c
+++ 5F/Socket/client.c
@@ -1,44 +1,45 @@
-#include <arpa/inet.h>
 #include <stdio.h>
 #include <string.h>
-#include <sys/socket.h>
 #include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 
 #define PORT 8080
 
 int main()
 {
-    int sock = 0, valread, client_fd;
     struct sockaddr_in serv_addr;
-    char *hello = "Hello from client";
+    int client, valread, sock = 0;
     char buffer[1024] = {0};
+    char *msg = "2) client: hello";
+
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
-        printf("\n Socket creation error \n");
+        printf("\nsocket failed\n");
         return -1;
     }
 
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
 
-    // Convert IPv4 and IPv6 addresses from text to binary
     if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0)
     {
-        printf("\nInvalid address/ Address not supported \n");
+        printf("\ninvalid address\n");
         return -1;
     }
 
-    if ((client_fd = connect(sock, (struct sockaddr *)&serv_addr,
-                             sizeof(serv_addr))) < 0)
+    if ((client = connect(sock, (struct sockaddr *)&serv_addr,
+                          sizeof(serv_addr))) < 0)
     {
-        printf("\nConnection Failed \n");
+        printf("\nconnection failed\n");
         return -1;
     }
-    send(sock, hello, strlen(hello), 0);
-    printf("Hello message sent\n");
+
+    send(sock, msg, strlen(msg), 0);
+    printf("1) client: message sent\n");
+
     valread = read(sock, buffer, 1024);
     printf("%s\n", buffer);
 
-    close(client_fd);
-    return 0;
+    close(client);
 }
