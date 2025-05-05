1188b9a2a361033f735374a0a1eacce86109b22b
alessiomodonesi <alessiomodo2004@gmail.com>
2025-05-05 20:41:00 +0200

    finito lab 7

--- unipd/lab7/mmapshared.c
+++ unipd/lab7/mmapshared.c
@@ -1,67 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <time.h>
+#include <string.h>
 
-#define FILENAME "testfile"
-#define FILESIZE (1024*1024*100) /* 100MB */
-
+#define FILENAME "sharedfile"
+#define FILESIZE 4096  // sufficiente per il messaggio "Hello world!"
 
 int main() {
-    pid_t pidA;
-
-    /* Create a test file */
-    int fd = open(FILENAME, O_CREAT | O_TRUNC | O_RDWR, S_IRUSR | S_IWUSR);
-    ftruncate(fd, FILESIZE); /* set the length of the file */
-
-    /* Map the file to memory */
-    char *data = mmap(NULL, FILESIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-
-    pidA = fork();
-    if (pidA == 0)
-    {
-	sprintf(fd, "Hello world!");
-	exit(0);
+    // Crea un file da condividere
+    int fd = open(FILENAME, O_CREAT | O_RDWR, 0666);
+    if (fd < 0) {
+        perror("open");
+        exit(1);
     }
 
-    /* Close the file */
+    // Allunga il file a FILESIZE
+    if (ftruncate(fd, FILESIZE) == -1) {
+        perror("ftruncate");
+        close(fd);
+        exit(1);
+    }
+
+    // Mappa il file in memoria condivisa
+    char *shared_mem = mmap(NULL, FILESIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    if (shared_mem == MAP_FAILED) {
+        perror("mmap");
+        close(fd);
+        exit(1);
+    }
+
+    // Chiude il file descriptor, mappatura già avvenuta
     close(fd);
 
-    /* Initialise the random number generator */
-    srand(time(NULL));
+    pid_t pid = fork();
+    if (pid < 0) {
+        perror("fork");
+        munmap(shared_mem, FILESIZE);
+        exit(1);
+    } else if (pid == 0) {
+        // Processo figlio
+        const char *msg = "Hello world!";
+        sprintf(shared_mem, "%s", msg);  // Scrive nella memoria condivisa
+        exit(0);  // Termina
+    } else {
+        // Processo padre
+        wait(NULL);  // Attende che il figlio termini
 
-    char byte;
+        // Legge e stampa il messaggio
+        printf("Messaggio dal figlio: %s\n", shared_mem);
 
-    /* Access the file randomly using mmap() */
-    clock_t start = clock(); /* start counting */
-    for (int i = 0; i < 1000000; i++) {
-        off_t offset = rand() % FILESIZE; /* generate random offset */
-        byte = data[offset]; /* read byte from memory mapped file */
+        // Pulizia
+        munmap(shared_mem, FILESIZE);
+        remove(FILENAME);
     }
-    clock_t end = clock(); /* stop counting */
-    printf("mmap() random access time: %.2f seconds\n", (double)(end - start) / CLOCKS_PER_SEC);
-
-
-
-    /* Access the file randomly using standard system calls */
-    fd = open(FILENAME, O_RDONLY); /* open the file */
-    start = clock(); /* start counting */
-    for (int i = 0; i < 1000000; i++) {
-        off_t offset = rand() % FILESIZE; /* generate random offset */
-        lseek(fd, offset, SEEK_SET); /* point to desired byte */
-        read(fd, &byte, 1); /* read byte from file */
-    }
-    end = clock(); /* stop counting */
-    printf("Standard system calls random access time: %.2f seconds\n", (double)(end - start) / CLOCKS_PER_SEC);
-
-
-
-    /* Clean up */
-    close(fd);
-    munmap(data, FILESIZE);
-    remove(FILENAME);
 
     return 0;
 }
