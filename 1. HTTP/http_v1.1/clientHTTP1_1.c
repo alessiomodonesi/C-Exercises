553f91f52f04823b208bf7accb3a51f8b7c14471
alessiomodonesi <alessiomodo2004@gmail.com>
2026-04-15 15:07:39 +0200

    Update clientHTTP1_1.c

--- 1. HTTP/http_v1.1/clientHTTP1_1.c
+++ 1. HTTP/http_v1.1/clientHTTP1_1.c
@@ -5,13 +5,13 @@
 #include <arpa/inet.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <fcntl.h>
 #include <sys/stat.h>
+#include <fcntl.h>
 
 int main()
 {
-    struct header // ricorda: l'ordine della variabili è importante
-    {
+    struct header
+    {            // ricorda: l'ordine della variabili è importante
         char *n; // header name
         char *v; // header value
     };
@@ -27,17 +27,18 @@ int main()
     address.sin_family = AF_INET;
     address.sin_port = htons(80); // big endian di 8080 (network byte order)
 
-    char *ip = (char *)&address.sin_addr.s_addr;
-
+    char *ip = (char *)&address.sin_addr;
     ip[0] = 217, ip[1] = 61, ip[2] = 15, ip[3] = 222; // www.example.it
 
     int c = connect(sockfd, (struct sockaddr *)&address, sizeof(address));
 
     if (c == 0)
-        printf("connection established\n");
+    {
+        printf("connessione stabilita\n");
+    }
     else
     {
-        perror("connection failed");
+        perror("connessione fallita");
         exit(1);
     }
 
@@ -49,96 +50,83 @@ int main()
     // Connection:keep-alive -> quando il server ha finito di inviare cose non chiude la connessione
     // ovviamente dopo l'inserimento di eventuali headers devo mettere CRLF CRLF
 
+    int byteScritti = 0;
     int m = 0;
-    int bytesWritten = 0;
-    while (bytesWritten < strlen(buffer))
+    while (byteScritti < strlen(buffer))
     {
-        m = write(sockfd, buffer + bytesWritten, strlen(buffer) - bytesWritten);
-        bytesWritten += m;
+        m = write(sockfd, buffer + byteScritti, strlen(buffer) - byteScritti);
+        byteScritti += m;
     }
+
+    printf("ho scritto %d bytes\n", byteScritti);
 
     char response[1000000];
 
     int n = 0;
-    int bytesRead = 0;
-    int headerIndex = 0;
-    int readHeaderName = 0;
     int byteBody = 0;
+    int headerIndex = 0;
+    int lettoNomeHeader = 0;
+    int byteLetti = 0;
 
     // lettura byte per byte con parsing interno
-    while ((n = read(sockfd, response + bytesRead, 1)) > 0)
-    {
-        for (int i = 0; i < bytesRead; i++)
-        {
-            if (response[i] == '\n' && response[i - 1] == '\r')
-            {
-                if (response[i - 3] == 0)
-                {
-                    body = response + i + 1;
-                    byteBody = bytesRead - (i + 1);
-                    break;
-                }
-
-                readHeaderName = 0;
-                response[i - 1] = '\0';
-                h[headerIndex].n = response + i + 1;
-            }
-            else if (!readHeaderName && response[i] == ':')
-            {
-                readHeaderName = 1;
-                response[i] = '\0';
-                h[headerIndex++].v = response + i + 1;
-            }
-        }
-        bytesRead += n;
-    }
-
-    statusLine = response;
-    headerIndex = 0; // resettiamo l'indice per il secondo ciclo di parsing post-lettura
-    readHeaderName = 0;
-
-    for (int i = 0; i < bytesRead; i++)
+    while ((n = read(sockfd, response + byteLetti, 1)) > 0)
     {
         // controllo se ho trovato la fine della status line (CRLF CRLF)
         // prima cerco il \n, se lo trovo controllo se prima c'è un \r, se c'è allora ho trovato la fine della status line
-        if (response[i] == '\n' && response[i - 1] == '\r')
+        if (response[byteLetti] == '\n' && response[byteLetti - 1] == '\r')
         {
-            if (response[i - 3] == 0)
+            if (response[byteLetti - 3] == 0)
             {
-                body = response + i + 1; // trovato il body
-                byteBody = bytesRead - (i + 1);
+                body = response + byteLetti + 1; // trovato il body
                 break;
             }
-
-            readHeaderName = 0;
-            response[i - 1] = '\0';              // metto il terminatore di stringa al posto del '\r'
-            h[headerIndex].n = response + i + 1; // salvato su h[0] il nome dell'header
+            lettoNomeHeader = 0;
+            response[byteLetti - 1] = 0;                 // metto il terminatore di stringa al posto del '\r'
+            h[headerIndex].n = response + byteLetti + 1; // salvato su h[0] il nome dell'header
         }
-        else if (!readHeaderName && response[i] == ':')
+        else if (!lettoNomeHeader && response[byteLetti] == ':')
         {
-            readHeaderName = 1;
-            response[i] = '\0';                    // metto il terminatore di stringa al posto del ':'
-            h[headerIndex++].v = response + i + 1; // salvato su h[0] il valore dell'header
+            lettoNomeHeader = 1;
+            response[byteLetti] = 0;                       // metto il terminatore di stringa al posto del ':'
+            h[headerIndex++].v = response + byteLetti + 1; // salvato su h[0] il valore dell'header
         }
+
+        byteLetti += n;
     }
 
-    char httpVersion[10];
+    printf("ho letto tutti gli header\n");
+    statusLine = response;
+
     int statusCode;
+    char httpVersion[10];
     char statusPhrase[20];
     sscanf(statusLine, "%s %d %s", httpVersion, &statusCode, statusPhrase); // prima specifico dove leggere, poi la formattazione della stringa
+    printf("lo status code = %d\n", statusCode);
 
-    printf("status code: %d\n", statusCode);
-    printf("number of headers read: %d\n", headerIndex);
+    printf("numero di header letti = %d\n", headerIndex - 1);
 
     for (int i = 0; i < headerIndex - 1; i++)
     {
         printf("header %s = %s\n", h[i].n, h[i].v); // stampo tutti gli header letti
+        if (strcmp("Content-Length", h[i].n) == 0)
+        {
+            sscanf(h[i].v, "%d", &byteBody);
+        }
     }
 
-    printf("responseLine: %s\n", statusLine);
-    printf("number of bytes in the body: %d\n", byteBody);
+    printf("il numero di caratteri del body = %d\n", byteBody);
+    // printf("valore del body:\n%s", body);
+    n = 0;
+    while ((n += read(sockfd, body + n, 1)) > 0 && n < byteBody)
+    {
+    }
 
-    int f = open("response.html", O_CREAT | O_WRONLY, 0644); // flag O_CREAT per creare il file se non esiste, aggiunto permessi 0644
+    close(sockfd);
+    printf("numero di byte letti = %d\n", n);
+    printf("la responseLine=%s\n", statusLine);
+
+    int f = open("response.html", O_CREAT | O_WRONLY, 0644); // flag O_CREAT per creare il file se non esiste, aggiunto permessi 0644 (Nota: nel tuo codice manca lo 0644 nella open, andrebbe aggiunto per compilare senza warning e settare i permessi correttamente)
+
     write(f, body, byteBody);
-    close(f);
+    // printf("il server dice:%s\n", response);
 }
